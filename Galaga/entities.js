aec8a24e9beee863bbb66c903285948b6bfbc08a
Álvaro Corrochano López <42788636+Corrochano@users.noreply.github.com>
2021-03-24 14:39:40 +0100

    Add files via upload

---
+++ Galaga/entities.js
@@ -0,0 +1,231 @@
+///////////////////////////////
+// SPRITE
+///////////////////////////////
+var Sprite = function() { }
+
+Sprite.prototype.setup = function(sprite,props) {
+    this.sprite = sprite;
+    this.merge(props);
+    this.frame = this.frame || 0;
+    this.w = SpriteSheet.map[sprite].w;
+    this.h = SpriteSheet.map[sprite].h;
+}
+Sprite.prototype.merge = function(props) {
+    if(props) {
+        for (var prop in props) {
+            this[prop] = props[prop];
+        }
+    }
+}
+
+Sprite.prototype.draw = function(ctx) {
+    SpriteSheet.draw(ctx,this.sprite,this.x,this.y,this.frame);
+}
+
+Sprite.prototype.hit = function(damage) {
+    this.board.remove(this);
+}
+
+var OBJECT_PLAYER = 1,
+    OBJECT_PLAYER_PROJECTILE = 2,
+    OBJECT_ENEMY = 4,
+    OBJECT_ENEMY_PROJECTILE = 8,
+    OBJECT_POWERUP = 16;
+
+///////////////////////////////
+// JUGADOR
+///////////////////////////////
+
+var PlayerShip = function() {
+    this.setup('ship_player', { vx: 0, frame: 0, reloadTime: 0.25, maxVel: 200 });
+
+    this.w = SpriteSheet.map['ship_player'].w;
+    this.h = SpriteSheet.map['ship_player'].h;
+    this.x = Game.width/2 - this.w / 2;
+    this.y = Game.height - 10 - this.h;
+    this.vx = 0;
+    this.maxVel = 200;
+
+    this.reloadTime = 0.25; // un cuarto de segundo
+    this.reload = this.reloadTime;
+
+    
+    this.step = function(dt) {
+        if(Game.keys['left']) { this.vx = -this.maxVel; }
+        else if(Game.keys['right']) { this.vx = this.maxVel; }
+        else { this.vx = 0; }
+        this.x += this.vx * dt;
+        if(this.x < 0) { this.x = 0; }
+        else if(this.x > Game.width - this.w) {
+            this.x = Game.width - this.w
+        }
+
+        this.reload-=dt;
+        if(Game.keys['fire'] && this.reload < 0) {
+            Game.keys['fire'] = false;
+            this.reload = this.reloadTime;
+
+            this.board.add(new PlayerMissile(this.x,this.y+this.h/2));
+            this.board.add(new PlayerMissile(this.x+this.w,this.y+this.h/2));
+        }
+
+    };
+
+    PlayerShip.prototype.hit = function(damage) {
+        if(this.board.remove(this)) {
+            loseGame();
+        }
+    }
+}
+
+PlayerShip.prototype = new Sprite();
+PlayerShip.prototype.type = OBJECT_PLAYER
+
+///////////////////////////////
+//PLAYER MISSILE
+///////////////////////////////
+
+//OK
+var PlayerMissile = function(x,y) {
+    this.setup('player_missile',{ vy: -700, damage: 10 });
+    this.x = x - this.w/2;
+    this.y = y - this.h;
+};  
+
+PlayerMissile.prototype = new Sprite();
+PlayerMissile.prototype.type = OBJECT_PLAYER_PROJECTILE;
+
+PlayerMissile.prototype.step = function(dt) {
+    this.y += this.vy * dt;
+    var collision = this.board.collide(this,OBJECT_ENEMY);
+    if(collision) {
+        collision.hit(this.damage);
+        this.board.remove(this);
+    } else if(this.y < -this.h) {
+        this.board.remove(this);
+    }
+};
+
+///////////////////////////////
+//ENEMIGOS
+///////////////////////////////
+
+var Enemy = function(blueprint,override) {
+    this.merge(this.baseParameters);
+    this.setup(blueprint.sprite,blueprint);
+    this.merge(override);
+}
+
+Enemy.prototype = new Sprite();
+Enemy.prototype.type = OBJECT_ENEMY;
+
+Enemy.prototype.baseParameters = { A: 0, B: 0, C: 0, D: 0, E: 0, F: 0, G: 0, H: 0, t: 0 }; 
+
+Enemy.prototype.step = function(dt) {
+    this.t += dt;
+    this.vx = this.A + this.B * Math.sin(this.C * this.t + this.D);
+    this.vy = this.E + this.F * Math.sin(this.G * this.t + this.H);
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+    if(this.y > Game.height || this.x < -this.w || this.x > Game.width) {
+        this.board.remove(this);
+    }
+    //Para ver si la nave colisiona con el jugador
+    var collision = this.board.collide(this,OBJECT_PLAYER);
+    if(collision) {
+        collision.hit(this.damage);
+        this.board.remove(this);
+    }
+}
+
+Enemy.prototype.hit = function(damage) {
+    this.health -= damage;
+    if(this.health <= 0)
+        this.board.remove(this);
+}
+    
+
+// NOS TOCA PÁGINA 25, Colisiones de los enemigos con el jugador
+
+///////////////////////////////
+//EXPLOSION
+///////////////////////////////
+
+var Explosion = function(centerX,centerY) {
+    this.setup('explosion', { frame: 0 });
+    this.x = centerX - this.w/2;
+    this.y = centerY - this.h/2;
+    this.subFrame = 0;
+};
+
+Explosion.prototype = new Sprite();
+
+Explosion.prototype.step = function(dt) {
+    this.frame = Math.floor(this.subFrame++ / 3);
+    if(this.subFrame >= 36) {
+        this.board.remove(this);
+    }
+};
+
+//y la explosión se crea cuando se destruye un enemigo:
+Enemy.prototype.hit = function(damage) {
+    this.health -= damage;
+    if(this.health <=0) {
+        if(this.board.remove(this)) {
+            this.board.add(new Explosion(this.x + this.w/2, this.y + this.h/2));
+        }
+    }
+}
+
+///////////////////////////////
+//LEVEL
+///////////////////////////////
+
+var Level = function(levelData,callback) {
+    this.levelData = [];
+    for(var i = 0; i < levelData.length; i++) {
+        this.levelData.push(Object.create(levelData[i]));
+    }
+    this.t = 0;
+    this.callback = callback;
+}
+
+Level.prototype.draw = function(ctx) { }
+
+Level.prototype.step = function(dt) {
+    var idx = 0, remove = [], curShip = null;
+
+    // Update the current time offset
+    this.t += dt * 1000;
+
+    // Example levelData
+    // Start, End, Gap, Type, Override
+    // [[ 0, 4000, 500, 'step', { x: 100 } ]
+    while((curShip = this.levelData[idx]) && 
+        (curShip[0] < this.t + 2000)) {
+            // Check if past the end time
+            if(this.t > curShip[1]) {
+                // If so, remove the entry
+                remove.push(curShip);
+            } else if(curShip[0] < this.t) {
+                // Get the enemy definition blueprint
+                var enemy = enemies[curShip[3]],
+                override = curShip[4];
+                // Add a new enemy with the blueprint and override
+                this.board.add(new Enemy(enemy,override));
+                // Increment the start time by the gap
+                curShip[0] += curShip[2];
+            }
+            idx++;
+    }
+    // Remove any objects from the levelData that have passed
+    for(var i = 0, len = remove.length; i < len; i++) {
+        var idx = this.levelData.indexOf(remove[i]);
+        if(idx != -1) this.levelData.splice(idx,1);
+    }
+    // If there are no more enemies on the board or in
+    // levelData, this level is done
+    if(this.levelData.length == 0 && this.board.cnt[OBJECT_ENEMY] == 0) {
+        if(this.callback) this.callback();
+    }
+}
---
+++ Galaga/game.js
@@ -0,0 +1,87 @@
+var startGame = function() {
+    Game.setBoard(0,new TitleScreen("Alien Invasion", "Press fire to start playing",
+    playGame));
+}
+
+var playGame = function() {
+    var board = new GameBoard();
+    board.add(new PlayerShip());
+    board.add(new Level(level1,winGame));
+    Game.setBoard(0,board); 
+};
+
+var winGame = function() {
+    //TODO 0?
+    Game.setBoard(0,new TitleScreen("You win!",
+    "Press fire to play again",
+    playGame));
+};
+
+var loseGame = function() {
+    //TODO 0?
+    Game.setBoard(0,new TitleScreen("You lose!",
+    "Press fire to play again",
+    playGame));
+    };
+
+var level1 = [
+    // Start, End, Gap, Type, Override
+    [ 0, 4000, 500, 'step' ],
+    [ 6000, 13000, 800, 'ltr' ],
+    [ 12000, 16000, 400, 'circle' ],
+    [ 18200, 20000, 500, 'straight', { x: 150 } ],
+    [ 18200, 20000, 500, 'straight', { x: 100 } ],
+    [ 18400, 20000, 500, 'straight', { x: 200 } ],
+    [ 22000, 25000, 400, 'wiggle', { x: 300 }],
+    [ 22000, 25000, 400, 'wiggle', { x: 200 }]
+];
+    
+var sprites = { 
+    ship_player: {sx:0, sy:0, w:37, h:40, frames:1},
+    ship_purple_enemy: {sx:37, sy:0, w:42, h:43, frames:1},
+    ship_orange_enemy: {sx:79, sy:0, w:37, h:43, frames:1},
+    ship_grey_enemy: {sx:116, sy:0, w:42, h:43, frames:1},
+    ship_green_enemy: {sx:158, sy:0, w:33, h:32, frames:1},
+    player_missile: {sx:0, sy:42, w:7, h:20, frames:1},
+    enemy_missile: {sx:9, sy:42, w:3, h:20, frames:1},
+    explosion: {sx:0, sy:70, w:64, h:46, frames:12},
+};
+
+var enemies = {
+    straight: { x: 0, y: -50, sprite: 'ship_grey_enemy', health: 10,
+            E: 100 },
+    ltr: { x: 0, y: -100, sprite: 'ship_purple_enemy', health: 10,
+            B: 200, C: 1, E: 200 },
+    circle: { x: 400, y: -50, sprite: 'ship_green_enemy', health: 10,
+            A: 0, B: -200, C: 1, E: 20, F: 200, G: 1, H: Math.PI/2 },
+    wiggle: { x: 100, y: -50, sprite: 'ship_orange_enemy', health: 20,
+            B: 100, C: 4, E: 100 },
+    step: { x: 0, y: -50, sprite: 'ship_green_enemy', health: 10,
+            B: 300, C: 1.5, E: 60 }
+    };
+
+// Indica que se llame al método de inicialización una vez
+// se haya terminado de cargar la página HTML
+// y este después de realizar la inicialización llamará a
+// startGame
+window.addEventListener("load", function() {
+    Game.initialize("game",sprites,startGame);
+});
+
+/*var callback = function(){
+        SpriteSheet.draw(ctx, "ship_player", 0,0,0);
+        SpriteSheet.draw(ctx, "ship_purple_enemy", 0,50,0);
+        SpriteSheet.draw(ctx, "ship_orange_enemy", 0,100,0);
+        SpriteSheet.draw(ctx, "ship_grey_enemy", 0,150,0);
+        SpriteSheet.draw(ctx, "ship_green_enemy", 0,200,0);
+        SpriteSheet.draw(ctx, "player_missile", 0,250,0);
+        SpriteSheet.draw(ctx, "enemy_missile", 0,300,0);
+        var i = 11;
+        timer = setInterval(function() {
+            SpriteSheet.draw(ctx, "explosion", 0,350,i)
+            i -= 1;
+            if(i == -1){
+                clearInterval(timer);
+            }
+          }, 16);
+    }*/
